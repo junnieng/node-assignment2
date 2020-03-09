3ef4603c9e7e091538514e4eab47119f1dd0d74b
Junnie <pknguyenkhanh@gmail.com>
2020-03-09 10:25:18 -0600

    final

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
-const gallery = require('./gallery')
+const gallery = require('./gallery');
 
 //Express module
 
@@ -11,34 +11,40 @@ app.set ('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: false}));
 
+app.use(express.static('public/images'));
+
 app.use(function (req, res, next) {
     res.locals.gallery= gallery;
     next();    
 });
 
-//Endpoint handlers 
+
+//Endpoint handlers for index(homepage), gallery page and each pic of gallery
 
 app.get ('/', function(req, res) {
     res.render('index', {title: 'Home'});
 });
 
 app.get ('/gallery', function(req, res) {
-    res.render('gallery', {title: 'Gallery'})
+    res.render('gallery', {title: 'Gallery'});
 });
 
-app.get ('/gallery/:id', function(req, res) {
+app.get ('/gallery/:id', function(req, res, next) {
     for (pic of gallery) {
-        if (pic.id == req.params.id) {
-            res.render('picid', {title: `${req.params.id}`});
+        if (pic.id == req.params.id ) {
+            res.render('galleryId', {title: `${req.params.id}`})
             return;
         }
     }  
     next();  
-})
+});
 
 
 //Serve static assets from a public direction:
 app.use(express.static(path.join(__dirname, 'public')));
+
+//The css styling was not working for the image endpoint (galleryId.ejs) so I looked online and found this solution
+app.use("/styles",express.static(__dirname + "/css"));
 
 // Moment code for current year in footer
 const moment = require('moment');
